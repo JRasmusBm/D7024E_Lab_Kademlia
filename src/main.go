8f9e26dd4c454a05716e5f812365843af477864d
Robin-rr <54886347+Robin-rr@users.noreply.github.com>
2019-09-05 10:47:47 +0200

    Adding docker-compose, print IP from main (#13)

--- src/main.go
+++ src/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"errors"
+)
 
 func main() {
-	fmt.Printf("Hello world!")
+	ip, err := getIP()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(ip)
+}
+
+func getIP() (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ip = ip.To4()
+			if ip == nil {
+				continue // not an ipv4 address
+			}
+			return ip.String(), nil
+		}
+	}
+	return "", errors.New("are you connected to the network?")
 }
