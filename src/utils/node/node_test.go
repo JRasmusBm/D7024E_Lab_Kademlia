016a529dad745f7e9f5bbf49855292ee9a1a4b5c
jrasmusbm <JRasmusBm@gmail.com>
2019-10-03 14:23:30 +0200

    Test utils/node
    Add error checks in hashing.NewKademliaID and hashing.ToKademliaID.
    
    Test the bucket module. The main method that was not covered elsewhere
    was that of adding a node to the bucket. There are two main cases to be
    aware of, one is when the node was already in the bucket, and the other
    when it is seen for the first time. Add tests for both.
    
    Add error checks in node that do not cause the program to crash. Improve
    error reporting. Change the node syntax to be node("<ID>","<IP").
    
    Test the node package, particularly the ToString, FromString and
    FromStrings methods.
    
    In the routing table there was one method that was untestable, since it
    was written in a way as to never hit the final case. Pull out the
    hashing.IDLength so that it can be set to 0 and thus hit the final
    statement. This is very much a hack and not useful, but it allows
    testing all lines of code.
    
    Test the routing table through the TableSynchronizer.

---
+++ src/utils/node/node_test.go
@@ -0,0 +1,123 @@
+package node
+
+import (
+	"fmt"
+	"testing"
+	"utils/constants"
+	"utils/hashing"
+)
+
+func TestToString(t *testing.T) {
+	id, _ := hashing.NewKademliaID("456")
+	node := Node{IP: "123", ID: id}
+	expected := fmt.Sprintf(`node("%s","123")`, node.ID)
+	actual := node.String()
+	if expected != actual {
+		t.Errorf("Expected %v got %v", expected, actual)
+	}
+}
+
+func TestFromString(t *testing.T) {
+	node, err := FromString(
+		"node(\"1231330000000000000000000000000000000001\",\"abc\")",
+	)
+	if err != nil {
+		t.Errorf("Should not throw error: %v", err)
+	}
+	expected := "1231330000000000000000000000000000000001"
+	actual := node.ID.String()
+	if actual != expected {
+		t.Errorf(
+			"Incorrect id, expected %v got %v",
+			expected,
+			actual,
+		)
+	}
+}
+
+func TestFromStringInvalidSyntax(t *testing.T) {
+	node, err := FromString(
+		"(\"1231330000000000000000000000000000000001\",\"abc\")",
+	)
+	if err == nil {
+		t.Errorf("Should throw error, instead got node: %#v", node)
+	}
+}
+
+func TestFromStringInvalidSyntax2(t *testing.T) {
+  node, err := FromString(
+    "node(\"1231330000000000000000000000000000000001\")",
+  )
+  if err == nil {
+    t.Errorf("Should throw error, instead got node: %#v", node)
+  }
+}
+
+
+func TestFromStringInvalidIDLength(t *testing.T) {
+	node, err := FromString(
+		"node(\"123\",\"abc\")",
+	)
+	if err == nil {
+		t.Errorf("Should throw error, instead got node: %#v", node)
+	}
+}
+
+func TestFromStringInvalidIDCharacters(t *testing.T) {
+	node, err := FromString(
+		"node(\"123133g000000000000000000000000000000001\",\"abc\")",
+	)
+	if err == nil {
+		t.Errorf("Should throw error, instead got node: %#v", node)
+	}
+}
+
+func TestFromStringsInvalidNodes(t *testing.T) {
+	nodes, err := FromStrings(
+		"node(\"123\",\"abc\") (123)",
+	)
+	if err == nil {
+		t.Errorf("Should throw error, instead got node: %#v", nodes)
+	}
+}
+func TestFromStrings(t *testing.T) {
+	nodes, err := FromStrings(
+		"node(\"0000000000000000000000000000000000000000\",\"abc\") " +
+			"node(\"1111111111111111111111111111111111111111\",\"def\")",
+	)
+	if err != nil {
+		t.Errorf("Should not throw error, but got: %v", err)
+	}
+	id1, _ := hashing.ToKademliaID("0000000000000000000000000000000000000000")
+	id2, _ := hashing.ToKademliaID("1111111111111111111111111111111111111111")
+	expected := [constants.CLOSESTNODES]Node{
+		Node{ID: id1, IP: "abc"},
+		Node{ID: id2, IP: "def"},
+	}
+	if len(nodes) != len(expected) ||
+		nodes[0].ID.String() != expected[0].ID.String() ||
+		nodes[0].IP != expected[0].IP ||
+		nodes[1].IP != expected[1].IP ||
+		nodes[1].ID.String() != expected[1].ID.String() {
+		t.Errorf("Expected len %v got len %v",
+			len(expected),
+			len(nodes),
+		)
+		t.Errorf("Position 0 ID: Expected %v got %v",
+			expected[0].ID,
+			nodes[0].ID,
+		)
+		t.Errorf("Position 0 IP: Expected %v got %v",
+			expected[0].IP,
+			nodes[0].IP,
+		)
+		t.Errorf("Position 1 ID: Expected %v got %v",
+			expected[1].ID,
+			nodes[1].ID,
+		)
+		t.Errorf("Position 1 IP: Expected %v got %v",
+			expected[1].IP,
+			nodes[1].IP,
+		)
+	}
+}
--- src/utils/node/routingtable_test.go
+++ src/utils/node/routingtable_test.go
@@ -1,23 +1,165 @@
 package node
 
 import (
-	"fmt"
 	"testing"
 	"utils/hashing"
 )
 
-func TestRoutingTable(t *testing.T) {
-	rt := NewRoutingTable(NewNode(hashing.NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
+func TestThreeClosest1(t *testing.T) {
+	id, _ := hashing.ToKademliaID("FFFFFFFF00000000000000000000000000000000")
+	root_node := NewNode(id, "localhost:8000")
+	id1, _ := hashing.ToKademliaID("FFFFFFFF00000000000000000000000000000000")
+	id2, _ := hashing.ToKademliaID("1111111100000000000000000000000000000000")
+	id3, _ := hashing.ToKademliaID("1111111200000000000000000000000000000000")
+	id4, _ := hashing.ToKademliaID("1111111300000000000000000000000000000000")
+	id5, _ := hashing.ToKademliaID("1111111400000000000000000000000000000000")
+	id6, _ := hashing.ToKademliaID("2111111400000000000000000000000000000000")
+	nodes := []Node{
+		NewNode(id1, "localhost:8001"),
+		NewNode(id2, "localhost:8002"),
+		NewNode(id3, "localhost:8003"),
+		NewNode(id4, "localhost:8004"),
+		NewNode(id5, "localhost:8005"),
+		NewNode(id6, "localhost:8006"),
+	}
+	rt := NewRoutingTable(root_node)
+	addNodes := make(chan AddNodeOp)
+	findClosestNodes := make(chan FindClosestNodesOp, 3)
+	go TableSynchronizer(rt, addNodes, findClosestNodes)
+	okChan := make(chan bool, 100)
+	for _, node := range nodes {
+		addNodes <- AddNodeOp{AddedNode: node, Resp: okChan}
+	}
 
-	rt.AddNode(NewNode(hashing.NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8001"))
-	rt.AddNode(NewNode(hashing.NewKademliaID("1111111100000000000000000000000000000000"), "localhost:8002"))
-	rt.AddNode(NewNode(hashing.NewKademliaID("1111111200000000000000000000000000000000"), "localhost:8002"))
-	rt.AddNode(NewNode(hashing.NewKademliaID("1111111300000000000000000000000000000000"), "localhost:8002"))
-	rt.AddNode(NewNode(hashing.NewKademliaID("1111111400000000000000000000000000000000"), "localhost:8002"))
-	rt.AddNode(NewNode(hashing.NewKademliaID("2111111400000000000000000000000000000000"), "localhost:8002"))
+	resp := make(chan []Node)
+	id7, _ := hashing.ToKademliaID("2111111400000000000000000000000000000000")
+	findClosestNodes <- FindClosestNodesOp{
+		Target: id7,
+		Count:  3,
+		Resp:   resp,
+	}
+	actual := <-resp
+	expected := []Node{nodes[5], nodes[4], nodes[3]}
+	if len(actual) != 3 ||
+		actual[0].ID != expected[0].ID ||
+		actual[1].ID != expected[1].ID ||
+		actual[2].ID != actual[2].ID {
+		t.Errorf("Position 0: Expected %v got %v", expected[0].ID, actual[0].ID)
+		t.Errorf("Position 1: Expected %v got %v", expected[1].ID, actual[1].ID)
+		t.Errorf("Position 2: Expected %v got %v", expected[2].ID, actual[2].ID)
+	}
+}
 
-	contacts := rt.FindClosestNodes(hashing.NewKademliaID("2111111400000000000000000000000000000000"), 20)
-	for i := range contacts {
-		fmt.Println(contacts[i].String())
+func TestThreeClosest2(t *testing.T) {
+	id, _ := hashing.ToKademliaID("FFFFFFFF00000000000000000000000000000000")
+	root_node := NewNode(id, "localhost:8000")
+	id1, _ := hashing.ToKademliaID("FFFFFFFF00000000000000000000000000000000")
+	id2, _ := hashing.ToKademliaID("1111111100000000000000000000000000000000")
+	id3, _ := hashing.ToKademliaID("1111111200000000000000000000000000000000")
+	id4, _ := hashing.ToKademliaID("1111111300000000000000000000000000000000")
+	id5, _ := hashing.ToKademliaID("1111111400000000000000000000000000000000")
+	id6, _ := hashing.ToKademliaID("2111111400000000000000000000000000000000")
+	nodes := []Node{
+		NewNode(id1, "localhost:8001"),
+		NewNode(id2, "localhost:8002"),
+		NewNode(id3, "localhost:8002"),
+		NewNode(id4, "localhost:8002"),
+		NewNode(id5, "localhost:8002"),
+		NewNode(id6, "localhost:8002"),
+	}
+	rt := NewRoutingTable(root_node)
+	addNodes := make(chan AddNodeOp)
+	findClosestNodes := make(chan FindClosestNodesOp, 3)
+	go TableSynchronizer(rt, addNodes, findClosestNodes)
+	okChan := make(chan bool, 100)
+	for _, node := range nodes {
+		addNodes <- AddNodeOp{AddedNode: node, Resp: okChan}
+	}
+
+	resp := make(chan []Node)
+	id7, _ := hashing.ToKademliaID("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")
+	findClosestNodes <- FindClosestNodesOp{
+		Target: id7,
+		Count:  3,
+		Resp:   resp,
+	}
+	actual := <-resp
+	expected := []Node{nodes[5], nodes[4], nodes[3]}
+	if len(actual) != 3 ||
+		actual[0].ID != expected[0].ID ||
+		actual[1].ID != expected[1].ID ||
+		actual[2].ID != actual[2].ID {
+		t.Errorf("Position 0: Expected %v got %v", expected[0].ID, actual[0].ID)
+		t.Errorf("Position 1: Expected %v got %v", expected[1].ID, actual[1].ID)
+		t.Errorf("Position 2: Expected %v got %v", expected[2].ID, actual[2].ID)
+	}
+}
+
+func TestAllClosest(t *testing.T) {
+	id, _ := hashing.ToKademliaID("FFFFFFFF00000000000000000000000000000000")
+	root_node := NewNode(id, "localhost:8000")
+	id1, _ := hashing.ToKademliaID("FFFFFFFF00000000000000000000000000000000")
+	id2, _ := hashing.ToKademliaID("1111111100000000000000000000000000000000")
+	id3, _ := hashing.ToKademliaID("1111111200000000000000000000000000000000")
+	id4, _ := hashing.ToKademliaID("1111111300000000000000000000000000000000")
+	id5, _ := hashing.ToKademliaID("1111111400000000000000000000000000000000")
+	id6, _ := hashing.ToKademliaID("2111111400000000000000000000000000000000")
+	nodes := []Node{
+		NewNode(id1, "localhost:8001"),
+		NewNode(id2, "localhost:8002"),
+		NewNode(id3, "localhost:8002"),
+		NewNode(id4, "localhost:8002"),
+		NewNode(id5, "localhost:8002"),
+		NewNode(id6, "localhost:8002"),
+	}
+	rt := NewRoutingTable(root_node)
+	addNodes := make(chan AddNodeOp)
+	findClosestNodes := make(chan FindClosestNodesOp)
+	go TableSynchronizer(rt, addNodes, findClosestNodes)
+	okChan := make(chan bool, 100)
+	for _, node := range nodes {
+		addNodes <- AddNodeOp{AddedNode: node, Resp: okChan}
+		<-okChan
+	}
+
+	resp := make(chan []Node)
+	id7, _ := hashing.ToKademliaID("2111111400000000000000000000000000000000")
+	findClosestNodes <- FindClosestNodesOp{
+		Target: id7,
+		Count:  20,
+		Resp:   resp,
+	}
+	actual := <-resp
+	expected := []Node{nodes[5], nodes[4], nodes[1], nodes[2], nodes[3], nodes[0]}
+	if len(actual) != 6 ||
+		actual[0].ID != expected[0].ID ||
+		actual[1].ID != actual[1].ID ||
+		actual[2].ID != actual[2].ID ||
+		actual[3].ID != actual[3].ID ||
+		actual[4].ID != actual[4].ID {
+		t.Errorf("Position 0: Expected %v got %v", expected[0].ID, actual[0].ID)
+		t.Errorf("Position 1: Expected %v got %v", expected[1].ID, actual[1].ID)
+		t.Errorf("Position 2: Expected %v got %v", expected[2].ID, actual[2].ID)
+		t.Errorf("Position 3: Expected %v got %v", expected[3].ID, actual[3].ID)
+		t.Errorf("Position 4: Expected %v got %v", expected[4].ID, actual[4].ID)
+		t.Errorf("Position 5: Expected %v got %v", expected[5].ID, actual[5].ID)
+	}
+}
+
+func TestGetInvalidBucketIndex(t *testing.T) {
+	id, _ := hashing.ToKademliaID("FFFFFFFF00000000000000000000000000000000")
+	root_node := NewNode(
+		id,
+		"localhost:8000",
+	)
+	rt := NewRoutingTable(root_node)
+	expected := hashing.IDLength*8 - 1
+	id1, _ := hashing.ToKademliaID("0000000000000000000000000000000000000000")
+	actual := rt.getBucketIndex(
+		id1,
+		0,
+	)
+	if expected != actual {
+		t.Errorf("Expected %d got %d", expected, actual)
 	}
 }
